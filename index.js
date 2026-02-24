4dc327394e0f412799c584de97e0b9502c8e0e0d
小巻葉音 <tirononi@gmail.com>
2026-02-24 13:46:07 +0900

    初めてのアップロード

---
+++ index.js
@@ -0,0 +1,65 @@
+//変数
+
+let currentStatus = "学習中";
+
+//定数
+const name = "Hello World dayo"
+
+//配列
+let suuji =['いち','に','さん','よん'];
+
+//ループ文
+//let index = 0
+//while(index < suuji.length){
+//    console.log(suuji[index]);
+//   //繰り返したい命令
+//    index++;
+//    //indexが＋１増えていく}
+
+
+
+// if else
+//if(suuji.length > 5){
+//    console.log('ボンバイエ');
+//}
+
+//else{
+//    console.log('ボンバ...');
+//}
+
+//関数
+const test = (arg) => {
+
+    //ここに実行したい命令を書く
+
+    if(inoki.length > arg){
+    console.log('ボンバイエ');
+}
+
+else{
+    console.log('ボンバ...');
+}
+
+};
+
+//test(3);
+//test(6);
+
+
+//オブジェクト
+const kawaii = {
+    color: 'pink',
+    size: 'large',
+    purfume: 'mint',
+}
+
+//console.log(window.innerHeight);
+
+//window.alert('yahhou')
+
+//console.log(document);
+
+document.getElementsByTagName('button')[0].addEventListener('click',( )=>{
+//命令
+window.alert('yahhou')
+});
